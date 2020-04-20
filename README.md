a5d96959ad645865a1e9245f086daeb60b7366db
wilhelmer <lars@wilhelmer.de>
2020-04-20 19:28:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# remove-markdown-links
+
+Removes Markdown links from selected text.
+
+A [Visual Studio Code](https://code.visualstudio.com/) extension to remove Markdown links from selected text.
+
+## Installation
+
+Search for `remove markdown` in the _Extensions_ section.
+
+## Usage
+
+Select the text from which all Markdown links should be removed and execute the `Remove Markdown Links` command via the command palette.
---
+++ src/extension.ts
@@ -0,0 +1,21 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('remove-markdown-links.removeLinks', () => {
+        console.log('Congratulations, your extension "remove-markdown-links" is now active!');
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+        text = text.replace(/(?:__|[*])|\[(.*?)\]\(.*?\)/g, "$1");
+        editor.edit(editBuilder => {
+            editBuilder.replace(selection, text);
+        });
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
