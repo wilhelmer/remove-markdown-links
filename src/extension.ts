1915b446f13c66891a400c8a276fe8973e67c7e8
Lars Wilhelmer <lars.wilhelmer@baslerweb.com>
2021-05-11 11:18:07 +0200

    Improved regex

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,13 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('remove-markdown-links.removeLinks', () => {
-        console.log('Congratulations, your extension "remove-markdown-links" is now active!');
         var editor = vscode.window.activeTextEditor;
         if (!editor) {
             return; // No open text editor
         }
         var selection = editor.selection;
         var text = editor.document.getText(selection);
-        text = text.replace(/([^!])\[(.*?)\]\(.*?\)/gm, "$1$2");
+        text = text.replace(/ \[(.*?)\]\(.*?\) /gm, " $1 ");
         editor.edit(editBuilder => {
             editBuilder.replace(selection, text);
         });
